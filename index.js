afd3366ab8216005075d611df83608bbfdab4d3a
uodyy <9mt123456789@gmail.com>
2026-06-10 09:56:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const { Client } = require('discord.js-selfbot-v13');
+const { joinVoiceChannel } = require('@discordjs/voice');
 const client = new Client();
 
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   // دخول الروم الصوتي
-  const channel = await client.channels.fetch('1414687975974895636');
-  if (channel) {
-    channel.join().catch(console.error);
-    console.log("Joined your voice channel!");
+  try {
+    const guild = client.guilds.cache.get('701688616614625360');
+    if (guild) {
+        joinVoiceChannel({
+            channelId: '1414687975974895636',
+            guildId: '701688616614625360',
+            adapterCreator: guild.voiceAdapterCreator,
+        });
+        console.log("Joined your voice channel successfully!");
+    }
+  } catch (error) {
+    console.error("Error joining channel:", error);
   }
 
-  // تحديث الحالة (Streaming)
+  // تحديث الحالة
   client.user.setActivity("Eyad", {
     type: "STREAMING",
     url: "https://www.twitch.tv/twitch"
